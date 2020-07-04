e44df4493734fe657040c78d8313e954f65a0fe7
text1 <xujinjie2001@outlook.com>
2020-07-04 19:07:28 +0800

    text

---
+++ form1/form1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonadd = new System.Windows.Forms.Button();
+            this.textBoxOp2 = new System.Windows.Forms.TextBox();
+            this.textBoxOp1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonadd
+            // 
+            this.buttonadd.BackColor = System.Drawing.Color.DarkRed;
+            this.buttonadd.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonadd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.buttonadd.Location = new System.Drawing.Point(334, 131);
+            this.buttonadd.Name = "buttonadd";
+            this.buttonadd.Size = new System.Drawing.Size(140, 97);
+            this.buttonadd.TabIndex = 0;
+            this.buttonadd.Text = "+";
+            this.buttonadd.UseVisualStyleBackColor = false;
+            this.buttonadd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxOp2
+            // 
+            this.textBoxOp2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxOp2.Location = new System.Drawing.Point(82, 203);
+            this.textBoxOp2.Name = "textBoxOp2";
+            this.textBoxOp2.Size = new System.Drawing.Size(181, 36);
+            this.textBoxOp2.TabIndex = 1;
+            // 
+            // textBoxOp1
+            // 
+            this.textBoxOp1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBoxOp1.Location = new System.Drawing.Point(82, 131);
+            this.textBoxOp1.Name = "textBoxOp1";
+            this.textBoxOp1.Size = new System.Drawing.Size(181, 36);
+            this.textBoxOp1.TabIndex = 2;
+            this.textBoxOp1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(383, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 3;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelResult.Location = new System.Drawing.Point(390, 71);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(90, 25);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOp1);
+            this.Controls.Add(this.textBoxOp2);
+            this.Controls.Add(this.buttonadd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonadd;
+        private System.Windows.Forms.TextBox textBoxOp2;
+        private System.Windows.Forms.TextBox textBoxOp1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ form1/form1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int op1Number = int.Parse(this.textBoxOp1.Text);
+                int op2Number = int.Parse(this.textBoxOp2.Text);
+                int result = op1Number + op2Number;
+                this.labelResult.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                this.labelResult.Text = ex.Message;
+            }
+        }
+    }
+}
